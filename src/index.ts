cc9ca35cade2ac08c79fed11a0116336d3e38f08
Marius Matulevicius <marius.matulevicius@twoday.com>
2023-07-26 10:08:09 +0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import express from "express";
-import cors from "cors";
-import config from "./util/config";
-import router from "./router";
+import { Server } from "http";
+import app from "./server";
+import { Request, Response } from "express";
 
-const app: express.Application = express();
+const server = new Server(app);
 
-app.use(cors());
-app.use(express.json());
-app.use(router);
-
-app.listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`);
-});
+export default (req: Request, res: Response) => {
+  server.emit("request", req, res);
+};
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import config from "./util/config";
+import router from "./router";
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+// app.listen(config.port, () => {
+//   console.log(`Server is running on port ${config.port}`);
+// });
+
+export default app;
